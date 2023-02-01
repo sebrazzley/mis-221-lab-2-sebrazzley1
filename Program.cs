ef9613dd2611a1bd1dff0591ef397c5329974044
sebrazzley <sebrazzley@crimson.ua.edu>
2023-02-01 02:06:05 -0600

    final

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+const double DRIVERS_WAGE = 1.27;
+double milesPerOrder;
+double ordersFilled;
+double milesDriven;
+double ordersMade;
+double costPerOrder;
+double fulfillmentRate;
+
+
+
+System.Console.WriteLine("Enter number of orders filled ");
+ordersFilled = int.Parse (Console.ReadLine());
+
+System.Console.WriteLine("Enter number of orders made ");
+ordersMade=int.Parse (Console.ReadLine());
+
+System.Console.WriteLine("Enter number of miles driven ");
+milesDriven = double.Parse (Console.ReadLine());
+
+fulfillmentRate = ordersFilled/ordersMade;
+milesPerOrder = milesDriven/ordersFilled;
+
+costPerOrder = milesPerOrder * DRIVERS_WAGE;
+
+System.Console.WriteLine("Your fulfillment rate is " + fulfillmentRate);
+System.Console.WriteLine("Your cost per order is " + costPerOrder);
+
